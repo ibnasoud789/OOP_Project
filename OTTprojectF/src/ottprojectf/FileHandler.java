6402521a6203b87634d9a93dc118b29598dbbf0a
ibnasoud789 <ibnasoud0210@gmail.com>
2023-12-01 02:21:58 +0600

    Soo na heiii

---
+++ OTTprojectF/src/ottprojectf/FileHandler.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ottprojectf;
+
+/**
+ *
+ * @author Asif
+ */
+public class FileHandler {
+    public static void main(String[] args){
+    System.out.println("It's my life");
+    
+}
+}
