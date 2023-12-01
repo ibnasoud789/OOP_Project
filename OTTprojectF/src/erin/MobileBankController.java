eca250f4255759f382dd68613df0a43d8b74cee0
erinnnnnnnnnnnn <151948665+erinnnnnnnnnnnn@users.noreply.github.com>
2023-12-01 16:20:54 +0600

    ..

--- OTTprojectF/src/erin/MobileBankController.java
+++ OTTprojectF/src/erin/MobileBankController.java
@@ -4,14 +4,20 @@
  */
 package erin;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -56,7 +62,12 @@ public class MobileBankController implements Initializable {
     }
 
     @FXML
-    private void backbuttonOnAction(ActionEvent event) {
+    private void backbuttonOnAction(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("earnfromContent.fxml"));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();           
     }
     
 }
