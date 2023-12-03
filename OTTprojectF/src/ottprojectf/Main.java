8e3e6d68d06ba03f8be4cc382171997276574a50
ibnasoud789 <ibnasoud0210@gmail.com>
2023-12-03 20:15:05 +0600

    rr

--- OTTprojectF/src/ottprojectf/Main.java
+++ OTTprojectF/src/ottprojectf/Main.java
@@ -24,7 +24,7 @@ public class Main extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("Netbanking.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("CommonScene.fxml"));
         
         Scene scene = new Scene(root);
         
