4635f641cadd13dbf53024a960dc0df7c9b99447
erinnnnnnnnnnnn <151948665+erinnnnnnnnnnnn@users.noreply.github.com>
2023-12-03 11:43:32 +0600

    shudhu matro ekta scene

---
+++ OTTprojectF/src/erin/ViewersController.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package erin;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+/**
+ * FXML Controller class
+ *
+ * @author ASUS
+ */
+public class ViewersController implements Initializable {
+
+    @FXML
+    private Button genreselection;
+    @FXML
+    private Button videoquality;
+    @FXML
+    private Button videoaccess;
+    @FXML
+    private Button watchadfree;
+    @FXML
+    private Button logoutbutton;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void genreselectionOnAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void videoqualityOnAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void videoaccessOnAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void watchadfreeOnAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void logoutbuttonOnAction(ActionEvent event) {
+    }
+    
+}
