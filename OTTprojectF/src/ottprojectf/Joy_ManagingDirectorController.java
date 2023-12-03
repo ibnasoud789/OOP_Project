825354621b0f2558537f4a4f3c7cc65dd616b9c1
ibnasoud789 <ibnasoud0210@gmail.com>
2023-12-03 21:19:11 +0600

    h

---
+++ OTTprojectF/src/ottprojectf/Joy_ManagingDirectorController.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package ottprojectf;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Asif
+ */
+public class Joy_ManagingDirectorController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void reviewContentButtonOnClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void deleteContentButtonOnClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void empListButtonOnClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void subAnalyticsButtonOnClicked(ActionEvent event) {
+    }
+
+    @FXML
+    private void financialReportButtonOnClick(ActionEvent event) {
+    }
+    
+}
